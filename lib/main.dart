6259589cb44c7a3a84cd885d0a37142bff0ec317
ftmhosseini <hosseini.ftm@gmail.com>
2021-10-21 13:29:09 +0330

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Than we setup preferred orientations,
+  // and only after it finished we run our app
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.landscapeLeft, DeviceOrientation.portraitDown]);
+  debugPaintSizeEnabled = false; // Set to true for visual layout
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: buildHomePage('Strawberry Pavlova Recipe'),
+    );
+  }
+
+  Widget buildHomePage(String title) {
+    const titleText = Padding(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        'Strawberry Pavlova',
+        style: TextStyle(
+          fontWeight: FontWeight.w800,
+          letterSpacing: 0.5,
+          fontSize: 30,
+        ),
+      ),
+    );
+
+    const subTitle = Text(
+      'Pavlova is a meringue-based dessert named after the Russian ballerina '
+          'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
+          'topped with fruit and whipped cream.',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontFamily: 'Georgia',
+        fontSize: 25,
+      ),
+    );
+
+    // #docregion ratings, stars
+    var stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        const Icon(Icons.star, color: Colors.black),
+        const Icon(Icons.star, color: Colors.black),
+      ],
+    );
+    // #enddocregion stars
+
+    final ratings = Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          stars,
+          const Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+    // #enddocregion ratings
+
+    // #docregion iconList
+    const descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+    // DefaultTextStyle.merge() allows you to create a default text
+    // style that is inherited by its child and all subsequent children.
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                const Text('PREP:'),
+                const Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.timer, color: Colors.green[500]),
+                const Text('COOK:'),
+                const Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                const Text('FEEDS:'),
+                const Text('4-6'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+    // #enddocregion iconList
+
+    // #docregion leftColumn
+    final leftColumn = Container(
+      padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: [
+          titleText,
+          subTitle,
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+    // #enddocregion leftColumn
+
+    final mainImage = Container(
+      width: 240.0,
+      child: Image.network(
+          'https://static01.nyt.com/images/2016/03/15/dining/15COOKING-STRAWBERRYPAVLOVA1/15COOKING-STRAWBERRYPAVLOVA1-articleLarge.jpg'),
+    );
+    // Image.asset(
+    //   'STRAWBERRYPAVLOVA.jpg',
+    //   width: 240,
+    //   fit: BoxFit.cover,
+    // );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      // #docregion body
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(0, 40, 0, 30),
+          height: 600,
+          child: ListView(
+            children: [
+              Card(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 360,
+                      child: leftColumn,
+                    ),
+                    mainImage,
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      // #enddocregion body
+    );
+  }
+}
